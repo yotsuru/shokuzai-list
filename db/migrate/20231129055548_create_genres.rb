f3af7f09352a36eddaf688c4f8a1729a7fc5bf2a
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-11-29 06:13:55 +0000

    [Genreモデル　genresテーブル　作成]

---
+++ db/migrate/20231129055548_create_genres.rb
@@ -0,0 +1,9 @@
+class CreateGenres < ActiveRecord::Migration[6.1]
+  def change
+    create_table :genres do |t|
+      t.references :user, null: false,foreign_key: true
+      t.string :name, null: false
+      t.timestamps
+    end
+  end
+end
