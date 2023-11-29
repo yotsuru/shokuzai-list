09b89b1f69ca8d70a5e2b691109bad5dca13efb2
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-11-29 05:03:51 +0000

    [ユーザー認証機能　実装]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "9b1ae85411cd42238c739dc368d1967e.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
