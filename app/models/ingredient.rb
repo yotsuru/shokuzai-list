1d057d4f5873318a2237284e4a1dd0ef1995a8e0
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-11-29 07:59:54 +0000

    [genreモデル ingredientモデル　記述]

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,8 @@
 class Ingredient < ApplicationRecord
     belongs_to :genre
 	belongs_to :user
+	
+	has_many :comments
     
     validates :genre_id, :user_id,:name, :quantity, presence: true
     validates :quantity, numericality: { only_float: true }
