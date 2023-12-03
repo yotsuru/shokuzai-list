c02e0948c1de7eb28f7a4f6ba7a29b2e6156261d
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-12-03 11:18:17 +0000

    [ジャンル 編集機能　実装]

--- app/controllers/users/genres_controller.rb
+++ app/controllers/users/genres_controller.rb
@@ -22,9 +22,13 @@ class Users::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
   
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to genre_path(genre.id)
   end
   
   def destroy
