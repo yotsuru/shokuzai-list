570075413661039af2632742914a01978fdec0ba
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-12-07 09:28:00 +0000

    [ingredient/index　並び替え機能　実装]

--- app/controllers/users/ingredients_controller.rb
+++ app/controllers/users/ingredients_controller.rb
@@ -3,6 +3,23 @@ class Users::IngredientsController < ApplicationController
   
   def index
     @ingredients = current_user.ingredients
+    
+    if params[:latest]#登録が新しい順
+     @ingredients = Ingredient.latest
+    elsif params[:old]#登録が古い順
+     @ingredients = Ingredient.old
+    elsif params[:purchase_date_latest]#購入日が新しい順
+     @ingredients = Ingredient.purchase_date_latest
+    elsif params[:purchase_date_old]#購入日が古い順
+     @ingredients = Ingredient.purchase_date_old
+    elsif params[:expiration_date_latest]#賞味期限が近い順
+     @ingredients = Ingredient.expiration_date_latest
+    elsif params[:expiration_date_old]#賞味期限が遠い順
+     @ingredients = Ingredient.expiration_date_old
+    else
+     @ingredients = Ingredient.all
+    end
+    
     @comment = Comment.new
   end
   
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -6,8 +6,20 @@ class Ingredient < ApplicationRecord
     
   validates :genre_id, :user_id, :name, :quantity, presence: true
   validates :quantity, numericality: { only_float: true }
-    
-     # 検索方法分岐
+  
+  # 並び替え機能
+  # 登録の新しい順、古い順
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  # 購入日の新しい順、古い順
+  scope :purchase_date_latest, -> {order(purchase_date: :desc)}
+  scope :purchase_date_old, -> {order(purchase_date: :asc)}
+  # 賞味期限の近い順、遠い順
+  scope :expiration_date_latest, -> {order(expiration_date: :desc)}
+  scope :expiration_date_old, -> {order(expiration_date: :asc)}
+  
+  
+  # 検索方法分岐
   def self.looks(search, name)
     if search == "perfect_match"
       @ingredient = Ingredient.where("name LIKE?", "#{name}")
